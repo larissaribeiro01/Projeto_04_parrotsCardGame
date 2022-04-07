2c40c707c43971158f7aa63127b13835cb6b3126
larissa <larissaribeabreu@gmail.com>
2022-04-06 23:27:06 -0300

    Prompt inicial

---
+++ scripts.js
@@ -0,0 +1,16 @@
+let numCartas=Number(prompt ("Com quantas cartas você quer jogar?"))
+let rest= numCartas%2
+while (rest!=0 || numCartas<4 || numCartas>14) {
+    numCartas=Number(prompt ("Com quantas cartas você quer jogar?"))
+    rest=numCartas%2
+}
+
+
+
+
+
+
+
+function turnCards (elemento) {
+
+}
